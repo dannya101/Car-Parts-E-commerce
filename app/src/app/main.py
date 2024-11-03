10357b08c9e12ac6db35e65cc6c7fd403c600780
rminsky42 <rminsky42@gmail.com>
2024-11-02 20:51:24 -0700

    Added generic endpoint for user

--- app/src/app/main.py
+++ app/src/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+
 from app.core.database import Base, engine
+from app.routes import api_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(api_router)
 
 @app.get("/")
 async def root():
