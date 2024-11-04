5d104292dd80a4a7a07b39281067d2e39d139827
rminsky42 <rminsky42@gmail.com>
2024-11-03 23:10:21 -0800

    Added simple authorization to main.py

--- app/src/app/main.py
+++ app/src/app/main.py
@@ -1,4 +1,12 @@
-from fastapi import FastAPI
+import jwt
+from jwt.exceptions import InvalidTokenError
+
+from passlib.context import CryptContext
+
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+from typing import Annotated
 
 from app.core.database import Base, engine
 from app.routes import api_router
@@ -9,7 +17,8 @@ app = FastAPI()
 
 app.include_router(api_router)
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
