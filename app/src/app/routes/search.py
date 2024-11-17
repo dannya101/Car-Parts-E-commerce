4067421dd047ac60f41c5917a77699651006e99d
rminsky42 <rminsky42@gmail.com>
2024-11-16 16:27:16 -0800

    Added simple search by name

--- app/src/app/routes/search.py
+++ app/src/app/routes/search.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from sqlalchemy.orm import Session
+
+import app.services.product as product_services
+from app.dependencies import get_db
 
 router = APIRouter()
 
 
 @router.get("/{search_terms}")
-def get_products_from_search():
-    pass
+def get_products_from_search(search_terms: str, db: Session = Depends(get_db)):
+    return product_services.get_product_by_name(db=db, name=search_terms)
