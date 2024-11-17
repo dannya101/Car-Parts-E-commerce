23e901271dc0690b0db719236d206bd5eca35597
dannya101 <124776050+dannya101@users.noreply.github.com>
2024-11-16 17:36:20 -0800

    added error condition to search

--- app/src/app/routes/search.py
+++ app/src/app/routes/search.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends,HTTPException
 
 from sqlalchemy.orm import Session
 
@@ -10,4 +10,10 @@ router = APIRouter()
 
 @router.get("/{search_terms}")
 def get_products_from_search(search_terms: str, db: Session = Depends(get_db)):
-    return product_services.get_product_by_name(db=db, name=search_terms)
+    result = product_services.get_product_by_name(db=db, name=search_terms)
+    if result is None:
+        raise HTTPException(
+            status_code=400,
+            detail="No Product Found!"
+        )
+    return result
