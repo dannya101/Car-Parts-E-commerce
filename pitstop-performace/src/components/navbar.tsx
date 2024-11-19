98fda99c66737a83688d14f1042895c5e15c0163
rminsky42 <rminsky42@gmail.com>
2024-11-19 05:34:47 -0800

    Added login and cart buttons

--- pitstop-performace/src/components/navbar.tsx
+++ pitstop-performace/src/components/navbar.tsx
@@ -1,17 +1,17 @@
-import Link from "next/link"
-import Image from "next/image";
+import Link from "next/link";
 import NavbarItem from "./navbaritem";
 
 export default function navbar() {
   const navLinks = [
     {label: "Home", link: "/"},
     {label: "About", link: "/about"},
-    {label: "Support", link: "/support"}
+    {label: "Support", link: "/support"},
+    {label: "Login", link: "/login"}
   ];
 
   return (
     <div className="bg-primary text-primary-foreground h-16 flex items-center">
-      <div className="container mx-auto flex justify-between">
+      <div className="container mx-auto flex justify-between pr-4 pl-4">
 
         {/*LOGO*/}
         <img src="/webname.svg" alt="logo" width={465} height={62}/>
@@ -24,7 +24,13 @@ export default function navbar() {
               navLink={navLink}
             />
           ))}
+
+        {/*CHECKOUT/CART*/}
+        <Link href="/checkout" className="flex items-center justify-center bg-primary p-2 rounded-full hover:bg-opacity-80">
+              <img src="/cart.svg" alt="Cart" width={30} height={30}/>
+        </Link>
         </nav>
+
       </div>
     </div>
   );
