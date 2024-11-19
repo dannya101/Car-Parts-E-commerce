86e8aec6e170ff4129fe901c944feb371b73d19c
rminsky42 <rminsky42@gmail.com>
2024-11-19 03:12:35 -0800

    added cycling background image

--- pitstop-performace/src/app/page.tsx
+++ pitstop-performace/src/app/page.tsx
@@ -2,6 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div></div>
+    <div className="relative min-h-screen">
+      <div className="absolute inset-0 fade-background"></div>
+      <div className="absolute inset-0 fade-background delay-2000"></div>
+      <div className="absolute inset-0 fade-background delay-4000"></div>
+
+      <div className="relative z-10 p-8">
+        <h1 className="text-white text-5xl text-border font-bold flex justify-center">Home Page</h1>
+      </div>
+    </div>
   );
 }
