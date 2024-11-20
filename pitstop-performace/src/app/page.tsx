709d3de9bf47638e9ff0941dd6cb990ebd0f2b6a
dannya101 <124776050+dannya101@users.noreply.github.com>
2024-11-19 23:13:50 -0800

    DRAFT: added two select buttons and trying to move home images

--- pitstop-performace/src/app/page.tsx
+++ pitstop-performace/src/app/page.tsx
@@ -1,31 +1,22 @@
 import Image from "next/image";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+import { SelectScrollable } from "@/components/selectbutton";
+
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen">
-      <div className="absolute inset-0 fade-background"></div>
-      <div className="absolute inset-0 fade-background delay-2000"></div>
-      <div className="absolute inset-0 fade-background delay-4000"></div>
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Theme" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="light">Light</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-          <SelectItem value="system">System</SelectItem>
-        </SelectContent>
-      </Select>
 
+    <div className="relative min-h-screen">
+        <div className="absolute top-1/2 right-20 w-96 h-96 fade-background animate-fade-in"></div>
+        <div className="absolute top-1/2 right-20 w-96 h-96 fade-background animate-fade-in delay-2000"></div>
+        <div className="absolute top-1/2 right-20 w-96 h-96 fade-background animate-fade-in delay-4000"></div>
       <div className="relative z-10 p-8">
         <h1 className="text-white text-5xl text-border font-bold flex justify-center">Home Page</h1>
+      </div>
+      <div className="relative top-1/3 z-10">
+        <SelectScrollable/>
+      </div>
+      <div className="top-1/3">
+        <SelectScrollable/>
       </div>
     </div>
   );
---
+++ pitstop-performace/src/components/selectbutton.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+export function SelectScrollable() {
+  return (
+    <Select>
+      <SelectTrigger className="w-[280px]">
+        <SelectValue placeholder="Make" />
+      </SelectTrigger>
+      <SelectContent>
+          <SelectItem value="Honda">Honda</SelectItem>
+          <SelectItem value="Ford">Ford</SelectItem>
+          <SelectItem value="Toyota">Toyota</SelectItem>
+          <SelectItem value="Lexus">Lexus</SelectItem>
+          <SelectItem value="Acura">Acura</SelectItem>
+          <SelectItem value="Tesla">Tesla</SelectItem>
+      </SelectContent>
+    </Select>
+  )
+}
