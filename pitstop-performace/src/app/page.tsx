c19d6a87215b46e3d6219698b0656fc71ec09ba7
dannya101 <124776050+dannya101@users.noreply.github.com>
2024-11-19 21:22:47 -0800

    adding a select button to home

--- pitstop-performace/src/app/page.tsx
+++ pitstop-performace/src/app/page.tsx
@@ -1,4 +1,11 @@
 import Image from "next/image";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export default function Home() {
   return (
@@ -6,6 +13,16 @@ export default function Home() {
       <div className="absolute inset-0 fade-background"></div>
       <div className="absolute inset-0 fade-background delay-2000"></div>
       <div className="absolute inset-0 fade-background delay-4000"></div>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
 
       <div className="relative z-10 p-8">
         <h1 className="text-white text-5xl text-border font-bold flex justify-center">Home Page</h1>
