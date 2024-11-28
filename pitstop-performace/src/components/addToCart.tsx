a2eb759dd43edf254b387663902e89771853bb45
dannya101 <124776050+dannya101@users.noreply.github.com>
2024-11-27 21:16:50 -0800

    need to figure out why authentication is not working

--- pitstop-performace/src/components/addToCart.tsx
+++ pitstop-performace/src/components/addToCart.tsx
@@ -12,43 +12,55 @@ export function AddToCartButton({ productId }: AddToCartButtonProps) {
   const [isPending, startTransition] = useTransition()
   const { toast } = useToast()
 
-  const handleAddToCart = () => {
-    startTransition(async () => {
+  const handleAddToCart = async(e: any) => {
+    e.preventDefault();
+    const token = sessionStorage.getItem("access_token");
+    if (!token) {
+        toast({
+        title: "Authentication Required",
+        description: "Please log in to add items to your cart.",
+        });
+        return;
+    }
+    // startTransition(async () => {
       try {
-        const token = sessionStorage.getItem("access_token");
-        if (!token) {
-            toast({
-            title: "Authentication Required",
-            description: "Please log in to add items to your cart.",
-            });
-            return;
-        }
         const response = await fetch("http://localhost:8000/cart/add", {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`
+                "Authorization": `Bearer ${token}`
             },
             body: JSON.stringify({
                 product_id: productId,
                 quantity: 1,
             }),
         })
-        toast({
-          title: "Success",
-          description: `Response ${response}`,
-        })
+        if(!response.ok) {
+          toast({
+              variant: "destructive",
+              title: "Failed to add to Cart",
+              description: "Try Again"
+            })
+          throw new Error(`Failed to add to Cart: ${response.statusText}`);
+        }
+        else
+        {
+          toast({
+            title: "Support Ticket Created",
+            description: "Ticket Has Been Created"
+        });
+        }
       } catch (error) {
         toast({
           title: "Error",
-          description: "Eror",
+          description: `${error}`,
         })
       }
-    })
-  }
+    }
+  // }
 
   return (
-    <Button onClick={handleAddToCart} disabled={isPending}>
+    <Button className="absolute bottom-4 left-4 z-10" onClick={handleAddToCart} disabled={isPending}>
       {isPending ? 'Adding...' : 'Add to Cart'}
     </Button>
   )
