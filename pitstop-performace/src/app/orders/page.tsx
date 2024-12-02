22c2afa4cc0ea9d6a47e66161d3d09802e7c94d2
rminsky42 <rminsky42@gmail.com>
2024-12-02 11:11:28 -0800

    Update orders page

--- pitstop-performace/src/app/orders/page.tsx
+++ pitstop-performace/src/app/orders/page.tsx
@@ -4,7 +4,6 @@ import { ListOrders } from "@/components/orders/listorders";
 export default function Orders() {
     return (
         <div className="p-6">
-            <h1 className="text-3xl font-bold mb-6">ORDERS</h1>
             <ListOrders />
         </div>
     );
